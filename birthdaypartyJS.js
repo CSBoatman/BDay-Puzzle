7408bd50ea96526ef1c0e020bd0325c1858ec738
Connor Boatman <connor@games-express.com>
2019-09-18 18:38:33 -0700

    fixed name skipping issue, removed unnecessary functions

--- birthdaypartyJS.js
+++ birthdaypartyJS.js
@@ -1,59 +1,55 @@
-//I want you to program a birthday party.
-//Create a class of partygoer and make a random number of guests.
-//Then create a class of present and assign a random number of presents to each guest.
+// BDay Party Generator
 
+//array list of guest names
 var guestNames = ["John","Rose","Dave","Jade","Jane","Roxy","Dirk","Jake","Mom","Dad"];
 
+//empty array list to hold guest names after they are used
 var usedNames = [];
 
-//name, address, how many presents
+//class to create Partygoer with name, address, and # of presents
 class Partygoer {
-	constructor(name, address) {
-		// idk why this is only assigning four names
-		for(let i = 0; i < guestNames.length; i++){
-			this.name = guestNames.shift();
-			usedNames.push(this.name);
-		};
+	constructor(address) {
+		this.name = guestNames.shift();
+		usedNames.push(this.name);
 		this.address = address;
 		this.presents = Math.floor(Math.random() * 5 + 1);
-	}
-}
+	};
+};
 
-//type, cost, wrapped or bagged
+//class to create Present(s) with a type and cost
 class Present {
-	constructor(type, cost) {
+	constructor() {
 		var wrapped = Math.floor(Math.random() * 2);
 		if(wrapped > 0){
 			this.type = "wrapped";
 		} else if(wrapped <= 0) {
 			this.type = "bagged";
-		}
+		};
 		this.cost = "$" + Math.floor(Math.random() * (101 - 1) + 1);
-	}
-}
+	};
+};
 
-function bdayparty(){
-	var guests = [];
-	for(let i = 0; i < 10; i++){
-		let person = new Partygoer("name","partyA");
-		guests.push(person);
-	}
-	return guests;
-}
+//empty array list to hold guest information
+var guests = [];
 
-function bdaygifts(){
-	var gifts = [];
-	for(let i = 0; i < 10; i++){
-		let goodie = new Present("type","cost");
-		gifts.push(goodie);
-	}
-	return gifts;
-}
+//for loop to add attendees to guest list
+for(let i = 0; i < 10; i++){
+	let person = new Partygoer("Home");
+	guests.push(person);
+};
 
-var partyA = bdayparty();
-var giftsA = bdaygifts();
-console.log(partyA);
-console.log(giftsA);
+//empty array list to hold gift information
+var gifts = [];
 
-// lets us see that it is running through all names in the original array
+//for loop to add goodies to gift list depending on number of attendees
+for(let i = 0; i < guests.length; i++){
+	let goodie = new Present();
+	gifts.push(goodie);
+};
+
+//print off the list of guests and gifts
+console.log(guests);
+console.log(gifts);
+
+//print off the list of used names to make sure they're all being used
 console.log(usedNames);
